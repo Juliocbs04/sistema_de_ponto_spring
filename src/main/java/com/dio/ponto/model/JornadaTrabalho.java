00694be3b4dcfdf5d429025ca0a8a3c5db9ed661
Juliocbs04 <profile.julio@gmail.com>
2021-07-31 14:23:07 -0300

    Aula 02 - Criacao do package Model e dentro dele criado o arquivo Jornada Trabalho e gerado seus métodos com Lombok

---
+++ src/main/java/com/dio/ponto/model/JornadaTrabalho.java
@@ -0,0 +1,15 @@
+package com.dio.ponto.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class JornadaTrabalho {
+    private long id;
+    private String descricao;
+
+}
